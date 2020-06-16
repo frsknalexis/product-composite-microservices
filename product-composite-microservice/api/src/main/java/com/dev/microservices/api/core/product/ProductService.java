f23e5fc311f1c56c941e2a71597b259ac97f3b78
Alexis Gutierrez <alexisgutierrezf.1997@gmail.com>
2020-06-16 16:53:29 -0500

    [adding lombok configuration]

--- product-composite-microservice/api/src/main/java/com/dev/microservices/api/core/product/ProductService.java
+++ product-composite-microservice/api/src/main/java/com/dev/microservices/api/core/product/ProductService.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface ProductService {
 
 	/**
-    * Sample usage: curl $HOST:$PORT/product/1
-    *
-    * @param productId
-    * @return Product, if found, else null
-    */
-	@GetMapping(value = "/product/{productId}",
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	 * Sample usage: curl $HOST:$PORT/product/1
+	 *
+	 * @param productId
+	 * @return Product, if found, else null
+	 */
+	@GetMapping(value = "/product/{productId}", 
+				produces = MediaType.APPLICATION_JSON_VALUE)
 	Product getProduct(@PathVariable(value = "productId") Integer productId);
 }
